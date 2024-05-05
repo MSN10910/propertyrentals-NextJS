13daca56d1e03576f66ba33975123f9bd65a0793
Joe <joadinmaina@gmail.com>
2024-05-05 16:31:17 +0300

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,23 @@
+import Hero from "@/components/Hero"
+import InfoBoxes from "@/components/InfoBoxes"
+import HomeProperties from "@/components/HomeProperties"
+import FeaturedProperties from "@/components/FeaturedProperties"
+
+
+
+const HomePage = () => {
+  
+
+  // console.log(process.env.MONGODB_URI);
+  return (
+    <>
+      <Hero />
+      <InfoBoxes />
+      <FeaturedProperties />
+      <HomeProperties />
+     
+    </>
+  )
+}
+
+export default HomePage
